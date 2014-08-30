9ce2d974f8aabd56b994aaca48e49202b4559a7a
Jean-Pascal THIERY <jpascal@lmarin.org>
2014-08-30 15:52:10 +0200

    Unit size

--- photo/model.py
+++ photo/model.py
@@ -3,13 +3,20 @@
 
 class Photo:
 	
-	def __init__(self, author, device, size):
+	def __init__(self, author, device, size, unit):
 		self.author = author
 		self.device = device
 		self.size = size
+		self.unitSize = unit
+
+class UnitSize:
+	
+	def __init__(self, name, shortName):
+		self.name = name
+		self.shortName = shortName
 
 if __name__ == "__main__":
 	
-	pA = Photo("JPascal", "D7000", 20)
-	print "Photo prise par {} avec {} [{} M]".format(pA.author, pA.device, pA.size)
+	pA = Photo("JPascal", "D7000", 20, UnitSize("Megaoctet", "Mo"))
+	print "Photo prise par {} avec {} [{} {}]".format(pA.author, pA.device, pA.size, pA.unitSize.shortName)
 
