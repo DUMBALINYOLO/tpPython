b263952600957b16dba01057238a5f5345568651
Jean-Pascal THIERY <jpascal@lmarin.org>
2014-08-30 16:14:43 +0200

    Album

--- photo/model.py
+++ photo/model.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 # -*-coding:Utf-8 -*
 
+class Album:
+	
+	def __init__(self, name):
+		self.name = name
+		self.photos = []
+
+	def addPhoto(self,photo):
+		self.photos.append(photo)
+
 class Photo:
 	
 	def __init__(self, author, device, size, unit):
@@ -19,4 +28,10 @@ if __name__ == "__main__":
 	
 	pA = Photo("JPascal", "D7000", 20, UnitSize("Megaoctet", "Mo"))
 	print "Photo prise par {} avec {} [{} {}]".format(pA.author, pA.device, pA.size, pA.unitSize.shortName)
+
+	albumVacances = Album("Vacances")
+	print "Il y a {} photos dans l'album".format(len(albumVacances.photos))
+	
+	albumVacances.addPhoto(pA)
+	print "Il y a {} photos dans l'album".format(len(albumVacances.photos))
 
